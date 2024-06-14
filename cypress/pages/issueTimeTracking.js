ba6435efccc62512a9e94949670989ebc6d3a504
RolandsTucs <rolands.tucs@gmail.com>
2024-06-15 00:38:25 +0300

    Committing issue-time-tracking code

---
+++ cypress/pages/issueTimeTracking.js
@@ -0,0 +1,16 @@
+// issueTimeTracking.js
+
+class IssueTimeTracking {
+  constructor() {
+    this.issueDetails = '[data-testid="modal:issue-details"]';
+    this.numberField = 'input[placeholder="Number"]';
+    this.stopWatchIcon = '[data-testid="icon:stopwatch"]';
+    this.placeHolderNumber = '[placeholder="Number"]';
+    this.modalTracking = '[data-testid="modal:tracking"]';
+    this.closeIcon = '[data-testid="icon:close"]';
+    this.backlogList = '[data-testid="board-list:backlog"]';
+  }
+}
+
+const issueTimeTracking = new IssueTimeTracking();
+export default issueTimeTracking;
